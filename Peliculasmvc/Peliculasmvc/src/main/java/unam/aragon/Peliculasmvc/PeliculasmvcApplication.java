a4b7a6288186c430b42e67d26826a63d742193ce
Ricardo Rojas <ricardorojas320@aragon.unam.mx>
2025-05-24 22:22:51 -0600

    Ricardo

---
+++ Peliculasmvc/Peliculasmvc/src/main/java/unam/aragon/Peliculasmvc/PeliculasmvcApplication.java
@@ -0,0 +1,13 @@
+package unam.aragon.Peliculasmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasmvcApplication.class, args);
+	}
+
+}
---
+++ Peliculasmvc/Peliculasmvc/src/test/java/unam/aragon/Peliculasmvc/PeliculasmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package unam.aragon.Peliculasmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
