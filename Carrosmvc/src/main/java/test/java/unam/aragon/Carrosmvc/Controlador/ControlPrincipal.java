f7c731e3e0beef82a0aa3f6ee679e799130c6521
Ricardo Rojas <ricardorojas320@aragon.unam.mx>
2025-06-04 18:28:32 -0600

    CR completos

--- Carrosmvc/src/main/java/test/java/unam/aragon/Carrosmvc/Controlador/ControlPrincipal.java
+++ Carrosmvc/src/main/java/test/java/unam/aragon/Carrosmvc/Controlador/ControlPrincipal.java
@@ -1,14 +1,16 @@
 package test.java.unam.aragon.Carrosmvc.Controlador;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import test.java.unam.aragon.Carrosmvc.Modelo.Carro;
 import test.java.unam.aragon.Carrosmvc.Servicios.CarroService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/Agencia/")
@@ -22,15 +24,29 @@ public class ControlPrincipal {
         return "index";
     }
 
-    @GetMapping("/Carro/")
-    public String Carro(Model model) {
-        Carro carro = new Carro(1, "Toyota", "Corolla", 2022, "XLE", "Rojo Metalico", "https://carsline.com.mx/wp-content/uploads/2024/03/01-55.jpg");
-        model.addAttribute("carro", carro);
-        return "Carro";
+    // Vista - modificar
+    @GetMapping("/vista/")
+    public String Vista(@RequestParam(required = false) Integer id, Model model) {
+        if (id != null) {
+            try {
+                model.addAttribute("CarroBuscado",carroService.getCarro(id) );
+            } catch (EntityNotFoundException e) {
+                model.addAttribute("error", "No se encontró un Carro con ID " + id);
+            }
+        }
+        return "VerCarro";
     }
 
+    @GetMapping("/buscarTodos")
+    public String mostrarTodosLosMuseos(Model model) {
+        List<Carro> carros = carroService.getTodo();
+        model.addAttribute("TodolosCarros", carros);
+        return "VerCarro";
+    }
+
+    // Crear
     @GetMapping("/Crear/")
-    public String formCrear(Model model) {
+    public String carros(Model model) {
         model.addAttribute("carro", new Carro());
         return "formCrear";
     }
@@ -45,10 +61,42 @@ public class ControlPrincipal {
         return "redirect:/Agencia/Crear/?exito";
     }
 
-    @GetMapping("/Carro/{id}")
-    public String Carro(@PathVariable int id, Model model) {
-        model.addAttribute("carro", carroService.getCarro(id));
-        return "Carro";
+    // Actualizar
+    @GetMapping("/modificar")
+    public String modificar(@RequestParam(name = "id", required = false) Integer id, Model model) {
+        model.addAttribute("carro", carroService.getTodo());
+        if (id != null) {
+            model.addAttribute("carro", carroService.getCarro(id));
+        } else {
+            model.addAttribute("carro", null); // Oculta el formulario si no se selecciono el museo
+        }
+        return "formActualizar";
     }
 
+
+    @PostMapping("/actualizar")
+    public String actualizar(@ModelAttribute Carro carro) {
+        LoggerFactory.getLogger(getClass()).info("Se actualizo el museo  'id': " + carro.getId());
+        LoggerFactory.getLogger(getClass()).info("Info guardada: " + carro);
+        carroService.actualizarCarro(carro);
+        return "redirect:/Agencia/modificar?exito&id=" + carro.getId();
+    }
+
+    // Borrar
+    @GetMapping("/borrar")
+    public String borrar(Model model) {
+        model.addAttribute("carros", carroService.getTodo());
+        return "borrar";
+    }
+    @PostMapping("/delete")
+    public String eliminar(@RequestParam Integer id) {
+        boolean eliminado = carroService.eliminarCarro(id);
+        if (eliminado) {
+            return "redirect:/Agencia/borrar?eliminado=true";
+        } else {
+            return "redirect:/Agencia/borrar?error=notfound";
+        }
+    }
 }
+
+
